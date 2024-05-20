79f8ec1079e55e9591bbf3ebc78aee7f6973ca8e
altuga <altuga@gmail.com>
2024-05-20 16:39:07 +0300

    goVirtual added

--- da-ch9-ex1/src/main/java/vt/MainVT.java
+++ da-ch9-ex1/src/main/java/vt/MainVT.java
@@ -9,10 +9,15 @@ public class MainVT {
 
     public static void main(String[] args) {
 
+        long startTime = System.currentTimeMillis();
         System.out.println("Starting...");
-        goPlatform();
-        //goVirtual();
+
+        //goPlatform();
+        goVirtual();
         System.out.println("Finished...");
+        long endTime = System.currentTimeMillis();
+        long loopTime = endTime - startTime;
+        System.out.println("Loop time: " + loopTime);
 
     }
 
@@ -31,7 +36,7 @@ public class MainVT {
 
     public static void goPlatform() {
         try (var executor = Executors.newFixedThreadPool(200)) {
-            IntStream.range(0, 10_0000).forEach(i -> {
+            IntStream.range(0, 10_000).forEach(i -> {
                 executor.submit(() -> {
                     Thread.sleep(Duration.ofSeconds(1));
                     return i;

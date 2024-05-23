7a9d03ba0936144ee7dc8e8b2ba510b1ccd183ec
altuga <altuga@gmail.com>
2024-05-23 20:48:52 +0300

    findTagsFromStackoverflow method is added

--- da-ch9-ex1/src/main/java/async/One.java
+++ da-ch9-ex1/src/main/java/async/One.java
@@ -9,13 +9,21 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 public class One {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
 
     public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
 
         ExecutorService executorService = Executors.newFixedThreadPool(2);
-        CompletableFuture<Integer> cf1 = CompletableFuture.supplyAsync(() -> process(1), executorService);
-        CompletableFuture<Integer> cf2 = CompletableFuture.supplyAsync(() -> process(2),executorService);
+        CompletableFuture<Integer> cf1 = CompletableFuture.supplyAsync(() -> findTagsFromStackoverflow("java"), executorService);
+        CompletableFuture<Integer> cf2 = CompletableFuture.supplyAsync(() -> findTagsFromStackoverflow("linux"), executorService);
 
         //cf1.get(); // blocking Thread
         CompletableFuture<Void> result = CompletableFuture.allOf(cf1, cf2); // signals that it is finished
@@ -33,5 +41,39 @@ public class One {
             throw new RuntimeException(e);
         }
         return value;
+    }
+
+
+    public static int findTagsFromStackoverflow(String param) {
+        String url = "https://stackoverflow.com/questions/tagged/"+param;
+
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // Set request header
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            int responseCode = con.getResponseCode();
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // Print result
+            System.out.println(response.length());
+            return response.length();
+
+        } catch (IOException e) {
+            System.out.println(" Error" + e.getMessage());
+            return 0;
+        }
     }
 }
